7563d43bdc311c2f3baf73dec0437e8129088bf9
Hermes <hermeschigoziri@gmail.com>
2022-01-23 13:28:59 +0100

    Add event handler for form submission

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-let main = document.querySelector('main');
+// VARIABLES
+let contactForm = document.getElementById('contact-form');
+let contactBtn = document.getElementById('contact-btn');
+let contactBtnImg = document.createElement('img');
+contactBtnImg.src="https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/50/000000/external-check-multimedia-kiranshastry-lineal-kiranshastry.png";
+
+// LOGIC
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    contactBtn.textContent = "";
+    contactBtn.appendChild(contactBtnImg);
+
+    // REVERT THE BTN TO ORIGINAL STATE AFTER ONE SEC
+    setTimeout(() => {
+        contactBtn.removeChild(contactBtnImg);
+        contactBtn.textContent = "Send";
+    }, 1000)  
+});
